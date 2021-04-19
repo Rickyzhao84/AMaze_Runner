8e0b1286ac0a7258a93881f9527fa67c3055ac97
ricky <pneumoniccoma@gmail.com>
2021-04-19 11:01:46 -0700

    Commit before making changes with namespace

--- tests/map_test.cc
+++ tests/map_test.cc
@@ -1,5 +1,20 @@
 #include <catch2/catch.hpp>
 
-TEST_CASE("Sanity Check") {
-    REQUIRE(1 == 1);
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#include "cinder/Log.h"
+#include "cinder/ImageIo.h"
+#include "cinder/gl/Texture.h"
+#include <visualizer/visualizer_app.h>
+#include <core/Location.h>
+#include <visualizer/visualizer_app.h>
+
+TEST_CASE("Map generates a different starting point each game") {
+    visualizer_app::visualizer::VisualizerApp();
+    
+}
+
+TEST_CASE("Map generates a different ending point each game") {
+    
 }
